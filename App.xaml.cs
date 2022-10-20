eb46c850c055b746734816c6fcd59cf171daddbc
S4pphiree <s4pph1r3sam@gmail.com>
2022-10-20 21:42:14 +0200

    Aggiungere i file di progetto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InterfacciaBanca;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace InterfacciaBanca;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
